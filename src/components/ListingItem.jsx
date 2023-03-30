1769e83c5885ffb09e89d907200d5855ccc0b8e1
Carl0sPineda <carlos7712p@gmail.com>
2023-03-29 21:41:32 -0600

    Allign items

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import Moment from "react-moment";
-import {MdLocationOn,MdEdit} from 'react-icons/md'
-import {FaTrash} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+// import Moment from "react-moment";
+// import { MdLocationOn, MdEdit } from 'react-icons/md'
+import { MdEdit } from 'react-icons/md'
+import { FaTrash } from 'react-icons/fa'
 
-export default function ListingItem({listing,id,onEdit,onDelete}) {
- 
+export default function ListingItem({ listing, id, onEdit, onDelete }) {
+
   function limitText(text) {
     const words = text.split(' ');
     if (words.length > 15) {
@@ -13,79 +14,79 @@ export default function ListingItem({listing,id,onEdit,onDelete}) {
     }
     return text;
   }
-  
+
   return (
     <>
-<li className="mr-1.5 ml-1.5">
-  <Link className="contents" to={`/category/${listing.type}/${id}`}>
-    <div className="property-card">
-      <figure className="card-banner">
-        {/* <a href="#"> */}
-        <img
-          loading="lazy"
-          src={listing.imgUrls[0]}
-          alt="propiedad"
-          className="w-100"
-        />
-        {/* </a> */}
-        <div className="card-badge green">Se Alquila</div>
-        <div className="banner-actions">
-          <button className="banner-actions-btn">
-            <ion-icon name="location"></ion-icon>
-            <address> {listing.address} </address>
-          </button>
-          <button className="banner-actions-btn">
-            <ion-icon name="camera"></ion-icon>
-            <span>{listing.imgUrls.length}</span>
-          </button>
-        </div>
-      </figure>
-      <div className="card-content">
-        <div className="card-price">
-          <strong>${listing.regularPrice}</strong>/Noche
-        </div>
-        {/* <h3 className="h3 card-title">
+      <li className="mr-1.5 ml-1.5">
+        <Link className="contents" to={`/category/${listing.type}/${id}`}>
+          <div className="property-card">
+            <figure className="card-banner">
+              {/* <a href="#"> */}
+              <img
+                loading="lazy"
+                src={listing.imgUrls[0]}
+                alt="propiedad"
+                className="w-100"
+              />
+              {/* </a> */}
+              <div className="card-badge green">Se Alquila</div>
+              <div className="banner-actions">
+                <button className="banner-actions-btn">
+                  <ion-icon name="location"></ion-icon>
+                  <address> {listing.address} </address>
+                </button>
+                <button className="banner-actions-btn">
+                  <ion-icon name="camera"></ion-icon>
+                  <span>{listing.imgUrls.length}</span>
+                </button>
+              </div>
+            </figure>
+            <div className="card-content">
+              <div className="card-price">
+                <strong>${listing.regularPrice}</strong>/Noche
+              </div>
+              {/* <h3 className="h3 card-title">
           <a href="#">Posee increibles vistas</a>
         </h3> */}
-        <p className="card-text">{limitText(listing.description)}</p>
-        <ul className="card-list">
-          <li className="card-item">
-            <strong>{listing.bedrooms}</strong>
-            
-            <ion-icon name="bed-outline"></ion-icon>
-            <span>Habitaciones</span>
-          </li>
-          <li className="card-item">
-          <strong>{listing.bathroms}</strong>
-            <ion-icon name="man-outline"></ion-icon>
-            <span>Baños</span>
-          </li>
-          {/* <li className="card-item">
+              <p className="card-text">{limitText(listing.description)}</p>
+              <ul className="card-list">
+                <li className="card-item">
+                  <strong>{listing.bedrooms}</strong>
+
+                  <ion-icon name="bed-outline"></ion-icon>
+                  <span>Habitaciones</span>
+                </li>
+                <li className="card-item">
+                  <strong>{listing.bathroms}</strong>
+                  <ion-icon name="man-outline"></ion-icon>
+                  <span>Baños</span>
+                </li>
+                {/* <li className="card-item">
               <strong>1500</strong>
               <ion-icon name="square-outline"></ion-icon>
               <span>m2</span>
             </li> */}
-        </ul>
-      </div>
-    </div>
-  </Link>
+              </ul>
+            </div>
+          </div>
+        </Link>
         <div className="card-footer">
-          <div className="card-footer-actions">
+          <div className="card-footer-actions flex justify-center">
             {onDelete && (
               <FaTrash
-              className=" right-2 h-[14px] cursor-pointer text-red-500"
+                className="ml-[84px] cursor-pointer text-red-500"
                 onClick={() => onDelete(listing.id)}
               />
             )}
             {onEdit && (
               <MdEdit
-              className=" bottom-2 right-7 h-4 cursor-pointer "
+                className="cursor-pointer  text-blue-900"
                 onClick={() => onEdit(listing.id)}
               />
             )}
           </div>
         </div>
-    </li>
+      </li>
     </>
   )
 }
