88551ba72561bd0b9e8049fe0009f7fee619db43
Carl0sPineda <carlos7712p@gmail.com>
2023-04-11 20:32:37 -0600

    filter for properties

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { toast } from "react-toastify";
 import {
   collection,
@@ -17,6 +17,16 @@ export default function Properties() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
+  const [searchBar, setSearchBar] = useState(" ");
+  const [cantBedrooms,setCantBendrooms]=useState(0);
+  const [cantBathrooms,setCantBathdrooms]=useState(0);
+  const [priceRange,setPriceRange]=useState(0);
+  const [width, setWidth] = useState(0);
+  const inputBed = useRef(null);
+  const inputBath = useRef(null);
+  const inputPrice = useRef(null);
+  const inputSearch = useRef(null);
+  
   useEffect(() => {
     async function fetchListings() {
       try {
@@ -25,11 +35,11 @@ export default function Properties() {
           listingRef,
           where("type", "==", "rent"),
           orderBy("timestamp", "desc"),
-          limit(8)
+          // limit(8)
         );
         const querySnap = await getDocs(q);
-        const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-        setLastFetchListing(lastVisible);
+        // const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+        // setLastFetchListing(lastVisible);
         const listings = [];
         querySnap.forEach((doc) => {
           return listings.push({
@@ -47,52 +57,188 @@ export default function Properties() {
     fetchListings();
   }, []);
 
-  async function onFetchMoreListings() {
-    try {
-      const listingRef = collection(db, "listings");
-      const q = query(
-        listingRef,
-        where("type", "==", "rent"),
-        orderBy("timestamp", "desc"),
-        startAfter(lastFetchedListing),
-        limit(4)
-      );
-      const querySnap = await getDocs(q);
-      const lastVisible = querySnap.docs[querySnap.docs.length - 1];
-      setLastFetchListing(lastVisible);
-      const listings = [];
-      querySnap.forEach((doc) => {
-        return listings.push({
-          id: doc.id,
-          data: doc.data(),
-        });
-      });
-      setListings((prevState)=>[...prevState, ...listings]);
-      setLoading(false);
-    } catch (error) {
-      toast.error("Could not fetch listing");
-    }
-  }
+  // async function onFetchMoreListings() {
+  //   try {
+  //     const listingRef = collection(db, "listings");
+  //     const q = query(
+  //       listingRef,
+  //       where("type", "==", "rent"),
+  //       orderBy("timestamp", "desc"),
+  //       startAfter(lastFetchedListing),
+  //       limit(4)
+  //     );
+  //     const querySnap = await getDocs(q);
+  //     const lastVisible = querySnap.docs[querySnap.docs.length - 1];
+  //     setLastFetchListing(lastVisible);
+  //     const listings = [];
+  //     querySnap.forEach((doc) => {
+  //       return listings.push({
+  //         id: doc.id,
+  //         data: doc.data(),
+  //       });
+  //     });
+  //     setListings((prevState)=>[...prevState, ...listings]);
+  //     setLoading(false);
+  //   } catch (error) {
+  //     toast.error("Could not fetch listing");
+  //   }
+  // }
+
 
   return (
     <div className="sm:mr-3 sm:ml-3 pt-4 space-y-6">
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Propiedades</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-2">Propiedades Disponibles</h1>
+
+      <div className="flex flex-col md:flex-row space-y-10 md:space-y-0 md:space-x-10" style={{justifyContent: "center", alignItems: "center"}}>
+
+          <div>
+            <label className="text-sm text-center mt-6 font-bold mb-2">Habitaciones </label>
+              <select className="cursor-pointer w-full md:w-auto" onChange={event =>{setCantBendrooms(event.target.value)}}>
+                <option value="">Sin filtro</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>     
+              </select>
+          </div>
+
+          <div>
+            <label className="text-sm text-center mt-6 font-bold mb-2">Baños </label>
+              <select className="cursor-pointer w-full md:w-auto" onChange={event =>{setCantBathdrooms(event.target.value)}}>
+                <option value="">Sin filtro</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>        
+              </select>
+          </div>
+
+          <div>
+          <input type="number" step={10}
+          placeholder="Filtrar por precio" onChange={event =>{{setPriceRange(event.target.value)}}}
+          className='w-small text-sm text-gray-700
+          bg-white border border-gray-400 rounded transition
+          duration-400 ease-in-out focus:text-gray-700 
+          focus:bg-white focus:border-slate-400'>
+          </input>
+          </div>
+
+          <div>
+          <input type="text" placeholder="Filtrar por ubicación" onChange={event =>{setSearchBar(event.target.value)}}
+          className='w-small px-4 py-2 text-sm text-gray-700
+          bg-white border border-gray-400 rounded transition
+          duration-400 ease-in-out focus:text-gray-700 
+          focus:bg-white focus:border-slate-400' style={{ borderRadius:100}}>
+          </input>
+          </div>
+
+      </div>
+      
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
         <>
           <main>
-            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-              {listings.map((listing) => (
+              {listings.filter((listing) =>{   //Se filtra por el numero de habitacions, ubicacion, precio
+
+                if(cantBedrooms == 0 && priceRange == 0 && cantBathrooms == 0 ){ //filtra unicamente por ubicacion
+
+                 return listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+                 
+                }else if(cantBedrooms == 0 && cantBathrooms == 0){ //filtra por precio o  ubicacion-precio
+                  return (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                }
+                else if(priceRange ==  0 && cantBathrooms == 0){ //filtra por habitacion o ubicacion-habitacion
+
+                  return listing.data.address.toLowerCase().includes(searchBar.toLowerCase()) && (listing.data.bedrooms == cantBedrooms)
+
+                }else if(cantBedrooms == 0 && priceRange == 0){ //filtra por baños o ubicacion-baños
+
+                  return (listing.data.bathroms == cantBathrooms) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+
+                }else if(priceRange == 0){ //filtra por habitaciones-baños o ubicacion-habitaciones-baños
+
+                  return (listing.data.bedrooms == cantBedrooms) && (listing.data.bathroms == cantBathrooms) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                }else if(cantBathrooms == 0){ //filtra por habitacions-precios o ubicacion-habitaciones-precios
+                  return (listing.data.bedrooms == cantBedrooms) && (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                }else if(cantBedrooms == 0){ //filtrar por banos-precio o ubicacion-banos-precio
+
+                  return (listing.data.bathroms == cantBathrooms) && (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                }else{ //filtra por todas las categorias
+                   return listing.data.address.toLowerCase().includes(searchBar.toLowerCase()) && cantBedrooms == listing.data.bedrooms && (parseInt(listing.data.regularPrice) <= priceRange)
+                   && (listing.data.bathroms == cantBathrooms)
+                }
+              }
+              ).map((listing) => (
                 <ListingItem
                   key={listing.id}
                   id={listing.id}
                   listing={listing.data}
                 />
-              ))}
-            </ul>
+              )).length > 0 ? ( //verifica que existan propiedades con los filtros ingresados 
+
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+
+                  {listings.filter((listing) =>{   //Se filtra por el numero de habitacions, ubicacion, precio
+
+                      if(cantBedrooms == 0 && priceRange == 0 && cantBathrooms == 0 ){ //filtra unicamente por ubicacion
+
+                      return listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+                      
+                      }else if(cantBedrooms == 0 && cantBathrooms == 0){ //filtra por precio o  ubicacion-precio
+                        return (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                      }
+                      else if(priceRange ==  0 && cantBathrooms == 0){ //filtra por habitacion o ubicacion-habitacion
+
+                        return listing.data.address.toLowerCase().includes(searchBar.toLowerCase()) && (listing.data.bedrooms == cantBedrooms)
+
+                      }else if(cantBedrooms == 0 && priceRange == 0){ //filtra por baños o ubicacion-baños
+
+                        return (listing.data.bathroms == cantBathrooms) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+
+                      }else if(priceRange == 0){ //filtra por habitaciones-baños o ubicacion-habitaciones-baños
+
+                        return (listing.data.bedrooms == cantBedrooms) && (listing.data.bathroms == cantBathrooms) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                      }else if(cantBathrooms == 0){ //filtra por habitacions-precios o ubicacion-habitaciones-precios
+                        return (listing.data.bedrooms == cantBedrooms) && (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                      }else if(cantBedrooms == 0){ //filtrar por banos-precio o ubicacion-banos-precio
+
+                        return (listing.data.bathroms == cantBathrooms) && (parseInt(listing.data.regularPrice) <= priceRange) && listing.data.address.toLowerCase().includes(searchBar.toLowerCase())
+
+                      }else{ //filtra por todas las categorias
+                        return listing.data.address.toLowerCase().includes(searchBar.toLowerCase()) && cantBedrooms == listing.data.bedrooms && (parseInt(listing.data.regularPrice) <= priceRange)
+                        && (listing.data.bathroms == cantBathrooms)
+                      }
+                      }
+                      ).map((listing) => (
+                      <ListingItem
+                        key={listing.id}
+                        id={listing.id}
+                        listing={listing.data}
+                      />
+                      ))}
+                  </ul>
+              ):(
+                <p className="text-center mt-2  mb-2">No hay propiedades disponibles con las caracteristicas ingresadas</p>
+              )
+
+              }
           </main>
-          {lastFetchedListing && (
+          
+          {/* {lastFetchedListing && (
             <div className="flex justify-center items-center">
               <button
                 onClick={onFetchMoreListings}
@@ -101,10 +247,12 @@ export default function Properties() {
                 Load more
               </button>
             </div>
-          )}
+          )} */}
         </>
       ) : (
-        <p>No se encuentran propiedades actualmente</p>
+        
+        <p>No se encuentran propiedades registradas actualmente</p>
+        
       )}
     </div>
   );
