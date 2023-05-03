ccbad3aa003db16fde9a22d54fabacb14500aa25
Carl0sPineda <carlos7712p@gmail.com>
2023-05-03 14:53:30 -0600

    emailjs and validation form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,22 +1,15 @@
 import { doc, getDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
-// import { useEffect } from "react";
 import { toast } from "react-toastify";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import Swal from 'sweetalert2';
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 import { db } from "../firebase";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function Contact({ userRef, listing }) {
   const [landlord, setLandlord] = useState(null);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [persons, setPersons] = useState("");
-  const [isFormComplete, setIsFormComplete] = useState(false);
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
-  const currentURL = window.location.href
+  const currentURL = window.location.href;
 
   useEffect(() => {
     async function getLandlord() {
@@ -31,126 +24,200 @@ export default function Contact({ userRef, listing }) {
     getLandlord();
   }, [userRef]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (name && email && phone && persons && startDate && endDate) {
-      const mailtoLink = `mailto:${landlord.email}?Subject=${listing.name}&body=Solicitud de reserva%0A%0ANombre:%20${name}%0AEmail: ${email}%0ATelefono: ${phone}%0ACantidad de personas:%20${persons}%0AFecha inicio: %20${startDate.toLocaleDateString("en-US")}%0AFecha fin: %20${endDate.toLocaleDateString("en-US")}%0A%0APropiedad solicitada:%20${currentURL}`;
-      // Redirige al usuario al cliente de correo electrónico predeterminado
-      window.location.href = mailtoLink;
-    } else {
-      Swal.fire({
-        title: 'Información',
-        text: 'Por favor completa todos los campos antes de enviar el formulario.',
-        icon: 'info',
-        width: "400px",
-
+  const handleSubmit = (values, { setSubmitting, resetForm }) => {
+    const templateParams = {
+      user_name: values.name,
+      user_email: values.email,
+      message: `Nombre: ${values.name}\nEmail: ${values.email}\nTelefono: ${values.phone}\nCantidad de personas: ${values.persons}\nFecha inicio: ${values.startDate}\nFecha fin: ${values.endDate}\n\nPropiedad solicitada: ${currentURL}`,
+    };
+    emailjs
+      .send(
+        import.meta.env.VITE_SERVICE_EMAILJS,
+        import.meta.env.VITE_TEMPLATE_EMAILJS,
+        templateParams,
+        import.meta.env.VITE_PUBLIC_KEY_EMAILJS
+      )
+      .then((result) => {
+        // console.log(result.text);
+      })
+      .catch((error) => {
+        // console.log(error.text);
       });
+    Swal.fire({
+      title: "Solicitud de reserva enviada",
+      text: "Gracias por su interés. Nos pondremos en contacto con usted lo antes posible para confirmar su reserva.",
+      icon: "success",
+      confirmButtonText: "Aceptar",
+    });
 
-    }
-  }
+    setSubmitting(false);
+    resetForm();
+  };
 
-
-
-  // useEffect(() => {
-  //   if (name && email && phone && persons && startDate && endDate) {
-  //     setIsFormComplete(true);
-  //     alert('Por favor complete todos los campos antes de enviar el formulario.')
-  //   } else {
-  //     setIsFormComplete(false);
-  //     // alert('Por favor complete todos los campos antes de enviar el formulario.')
-  //   }
-  // }, [name, email, phone, persons, startDate, endDate]);
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required("El nombre es requerido"),
+    email: Yup.string()
+      .email("El correo electrónico es inválido")
+      .required("El correo electrónico es requerido"),
+    phone: Yup.string().required("El teléfono es requerido"),
+    persons: Yup.number()
+      .required("La cantidad de personas es requerida")
+      .min(1, "La cantidad de personas debe ser mayor a 0"),
+    startDate: Yup.date().required("La fecha de inicio es requerida"),
+    endDate: Yup.date()
+      .min(
+        Yup.ref("startDate"),
+        "La fecha de fin debe ser posterior a la fecha de inicio"
+      )
+      .required("La fecha de fin es requerida"),
+  });
 
   return (
     <>
       {landlord !== null && (
-        <div className="flex flex-col w-full">
-          <div className="mb-6">
-            <label htmlFor="name" className="block text-gray-700 font-bold mb-2">
-              Nombre
-            </label>
-            <input
-              type="text"
-              name="name"
-              id="name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
+        <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            phone: "",
+            persons: "",
+            startDate: "",
+            endDate: "",
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className="flex flex-col w-full">
+            <div className="mb-6">
+              <label
+                htmlFor="name"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Nombre
+              </label>
+              <Field
+                type="text"
+                name="name"
+                id="name"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
-              Email
-            </label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
+            <div className="mb-6">
+              <label
+                htmlFor="email"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Correo electrónico
+              </label>
+              <Field
+                type="email"
+                name="email"
+                id="email"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label htmlFor="phone" className="block text-gray-700 font-bold mb-2">
-              Telefono
-            </label>
-            <input
-              type="text"
-              name="phone"
-              id="phone"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
+            <div className="mb-6">
+              <label
+                htmlFor="phone"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Teléfono
+              </label>
+              <Field
+                type="tel"
+                name="phone"
+                id="phone"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label htmlFor="persons" className="block text-gray-700 font-bold mb-2">
-              Cantidad de personas
-            </label>
-            <input
-              type="text"
-              name="persons"
-              id="persons"
-              value={persons}
-              onChange={(e) => setPersons(e.target.value)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
+            <div className="mb-6">
+              <label
+                htmlFor="persons"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Cantidad de personas
+              </label>
+              <Field
+                type="number"
+                name="persons"
+                id="persons"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="persons"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-          <div className="mb-6">
-            <label htmlFor="startDate" className="block text-gray-700 font-bold mb-2">
-              Fecha de inicio
-            </label>
-            <DatePicker
-              name="startDate"
-              id="startDate"
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="endDate" className="block text-gray-700 font-bold mb-2">
-              Fecha de fin
-            </label>
-            <DatePicker
-              name="endDate"
-              id="endDate"
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-              className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
-            />
-          </div>
+            <div className="mb-6">
+              <label
+                htmlFor="startDate"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Fecha de inicio
+              </label>
+              <Field
+                type="date"
+                name="startDate"
+                id="startDate"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="startDate"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-          <button onClick={handleSubmit} className="px-7 py-3 bg-slate-600 text-white rounded text-sm uppercase shadow-md hover:bg-slate-700 hover:shadow-lg focus:bg-slate-700 focus:shadow-lg active:bg-slate-800 active:shadow-lg transition duration-150 ease-in-out w-full text-center mb-6" type="button">
-            Enviar
-          </button>
+            <div className="mb-6">
+              <label
+                htmlFor="endDate"
+                className="block text-gray-700 font-bold mb-2"
+              >
+                Fecha de fin
+              </label>
+              <Field
+                type="date"
+                name="endDate"
+                id="endDate"
+                className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600"
+              />
+              <ErrorMessage
+                name="endDate"
+                component="div"
+                className="text-red-600"
+              />
+            </div>
 
-        </div>
+            <div className="mt-8">
+              <button
+                type="submit"
+                className="w-full px-6 py-3 font-bold text-center text-white bg-slate-600 rounded hover:bg-slate-500 focus:outline-none focus:shadow-outline-slate active:bg-slate-700"
+              >
+                Enviar
+              </button>
+            </div>
+          </Form>
+        </Formik>
       )}
     </>
   );
