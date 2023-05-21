43447044e5abbb827927fcaab02798b5d694be4f
Carl0sPineda <carlos7712p@gmail.com>
2023-05-20 21:57:58 -0600

    design

--- src/App.jsx
+++ src/App.jsx
@@ -18,23 +18,26 @@ function App() {
   return (
     <>
       <Router>
-    <Header/>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<PrivateRoute/>}>
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/properties" element={<Properties />} />
           <Route path="/category/:categoryName" element={<Category />} />
-          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
-          <Route path="/create-listing" element={<PrivateRoute/>}>
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/category/:categoryName/:listingId"
+            element={<Listing />}
+          />
+          <Route path="/create-listing" element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
           </Route>
-          <Route path="/edit-listing" element={<PrivateRoute/>}>
-          <Route path="/edit-listing/:listingId" element={<EditListing />} />
+          <Route path="/edit-listing" element={<PrivateRoute />}>
+            <Route path="/edit-listing/:listingId" element={<EditListing />} />
           </Route>
         </Routes>
       </Router>
