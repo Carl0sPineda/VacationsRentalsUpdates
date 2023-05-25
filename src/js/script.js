738aaa44e54626e2446ce1bde24f825d7f2491d9
Carl0sPineda <carlos7712p@gmail.com>
2023-05-24 21:53:48 -0600

    delete js

--- src/js/script.js
+++ src/js/script.js
@@ -1,32 +1,32 @@
-"use strict";
+// "use strict";
 
-/**
- * element toggle function
- */
+// /**
+//  * element toggle function
+//  */
 
-const elemToggleFunc = function (elem) {
-  elem.classList.toggle("active");
-};
+// const elemToggleFunc = function (elem) {
+//   elem.classList.toggle("active");
+// };
 
-/**
- * navbar toggle
- */
+// /**
+//  * navbar toggle
+//  */
 
-const navbar = document.querySelector("[data-navbar]");
-const overlay = document.querySelector("[data-overlay]");
-const navCloseBtn = document.querySelector("[data-nav-close-btn]");
-const navOpenBtn = document.querySelector("[data-nav-open-btn]");
-const navbarLinks = document.querySelectorAll("[data-nav-link]");
+// const navbar = document.querySelector("[data-navbar]");
+// const overlay = document.querySelector("[data-overlay]");
+// const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+// const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+// const navbarLinks = document.querySelectorAll("[data-nav-link]");
 
-const navElemArr = [overlay, navCloseBtn, navOpenBtn];
+// const navElemArr = [overlay, navCloseBtn, navOpenBtn];
 
-/**
- * close navbar when click on any navbar link
- */
+// /**
+//  * close navbar when click on any navbar link
+//  */
 
-for (let i = 0; i < navbarLinks.length; i++) {
-  navElemArr.push(navbarLinks[i]);
-}
+// for (let i = 0; i < navbarLinks.length; i++) {
+//   navElemArr.push(navbarLinks[i]);
+// }
 
 /**
  * addd event on all elements for toggling navbar
