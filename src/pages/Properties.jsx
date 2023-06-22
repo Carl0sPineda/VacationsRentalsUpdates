f03cdd48b7517ea2ff7106c975f19897670c2f1c
Carl0sPineda <carlos7712p@gmail.com>
2023-06-21 20:10:15 -0600

    aos add again

--- src/pages/Properties.jsx
+++ src/pages/Properties.jsx
@@ -12,6 +12,9 @@ import {
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
+import Footer from "../components/Footer";
+import Aos from "aos";
+import "aos/dist/aos.css";
 
 export default function Properties() {
   const [listings, setListings] = useState(null);
@@ -21,8 +24,6 @@ export default function Properties() {
   const [cantBedrooms, setCantBendrooms] = useState(0);
   const [cantBathrooms, setCantBathdrooms] = useState(0);
   const [priceRange, setPriceRange] = useState(0);
- 
- 
 
   useEffect(() => {
     async function fetchListings() {
@@ -54,115 +55,120 @@ export default function Properties() {
     fetchListings();
   }, []);
 
+  useEffect(() => {
+    Aos.init({
+      duration: 1500,
+      disable: window.innerWidth < 768,
+    });
+  }, []);
+
   if (loading) {
     return <Spinner />;
   }
 
   return (
-    <div className="sm:mr-3 sm:ml-5 pt-4 space-y-6 mt-2">
+    <div
+      className="sm:mr-3 sm:ml-5 pt-4 space-y-6 mt-2"
+      data-aos="fade-right"
+      data-aos-easing="linear"
+    >
       {/* <h1 className="text-3xl text-center mt-6 font-bold mb-2">Propiedades Disponibles</h1> */}
 
       <div className="flex md:flex-row space-y-2 md:space-y-0 md:space-x-10 justify-center sm: flex-col ml-2 mb-6">
         <div className="flex sm:flex space-x-8 justify-center">
-        <div className="flex flex-col mt-2 md:flex-row md:items-center">
-          <label className="text-lg text-center font-bold mr-4">
-            Habitaciones
-          </label>
-          <div className="text-center">
-
-          <select
-            className="cursor-pointer text-sm text-gray-700
-                 bg-white border border-gray-400 rounded transition
-                  duration-400 ease-in-out focus:text-gray-700 
-                  focus:bg-white focus:border-slate-400 w-40 md:w-32"
-            onChange={(event) => {
-              setCantBendrooms(event.target.value);
-            }}
-            style={{ borderRadius: 100 }}
-          >
-            <option value="">Sin filtro</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-          </select>
+          <div className="flex flex-col mt-2 md:flex-row md:items-center">
+            <label className="text-lg text-center font-bold mr-4">
+              Habitaciones
+            </label>
+            <div className="text-center">
+              <select
+                className="cursor-pointer text-sm text-gray-700
+               bg-white border border-gray-400 rounded transition
+                duration-400 ease-in-out focus:text-gray-700 
+                focus:bg-white focus:border-slate-400 w-40 md:w-32"
+                onChange={(event) => {
+                  setCantBendrooms(event.target.value);
+                }}
+                style={{ borderRadius: 100 }}
+              >
+                <option value="">Sin filtro</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col mt-2 md:flex-row md:items-center">
-          <label className="text-lg text-center font-bold mr-4">
-            Baños
-          </label>
-          <div className="text-center">
-          <select
-            className="cursor-pointer text-sm text-gray-700
-            bg-white border border-gray-400 rounded transition
-             duration-400 ease-in-out focus:text-gray-700 
-             focus:bg-white focus:border-slate-400 w-40 md:w-32"
-            onChange={(event) => {
-              setCantBathdrooms(event.target.value);
-            }}
-            style={{ borderRadius: 100 }}
-          >
-            <option value="">Sin filtro</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-          </select>
+          <div className="flex flex-col mt-2 md:flex-row md:items-center">
+            <label className="text-lg text-center font-bold mr-4">Baños</label>
+            <div className="text-center">
+              <select
+                className="cursor-pointer text-sm text-gray-700
+          bg-white border border-gray-400 rounded transition
+           duration-400 ease-in-out focus:text-gray-700 
+           focus:bg-white focus:border-slate-400 w-40 md:w-32"
+                onChange={(event) => {
+                  setCantBathdrooms(event.target.value);
+                }}
+                style={{ borderRadius: 100 }}
+              >
+                <option value="">Sin filtro</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+              </select>
+            </div>
           </div>
-        </div>
         </div>
 
         <div className="flex sm:flex space-x-8 justify-center">
-        <div className="flex flex-col mt-2 md:flex-row md:items-center">
-          <label className="text-lg text-center font-bold mr-4">
-            Precio
-          </label>
+          <div className="flex flex-col mt-2 md:flex-row md:items-center">
+            <label className="text-lg text-center font-bold mr-4">Precio</label>
 
-          <div className="text-center">
-          <input
-            type="number"
-            step={10}
-            placeholder="Filtrar $/Noche"
-            onChange={(event) => {
-              {
-                setPriceRange(event.target.value);
-              }
-            }}
-            className="cursor-pointer text-sm text-gray-700
-            bg-white border border-gray-400 rounded transition
-             duration-400 ease-in-out focus:text-gray-700 
-             focus:bg-white focus:border-slate-400 w-40 md:w-32"
-            style={{ borderRadius: 100 }}
-          ></input>
+            <div className="text-center">
+              <input
+                type="number"
+                step={10}
+                placeholder="Menor a $"
+                onChange={(event) => {
+                  {
+                    setPriceRange(event.target.value);
+                  }
+                }}
+                className="cursor-pointer text-sm text-gray-700
+          bg-white border border-gray-400 rounded transition
+           duration-400 ease-in-out focus:text-gray-700 
+           focus:bg-white focus:border-slate-400 w-40 md:w-32"
+                style={{ borderRadius: 100 }}
+              ></input>
+            </div>
           </div>
-        </div>
 
-        <div className="flex flex-col mt-2 md:flex-row md:items-center">
-          <label className="text-lg text-center font-bold mr-4">
-            Ubicación
-          </label>
-          <div className="text-center">
-          <input
-            type="text"
-            placeholder="Filtrar Ubicación"
-            onChange={(event) => {
-              setSearchBar(event.target.value);
-            }}
-            className="cursor-pointer text-sm text-gray-700
-            bg-white border border-gray-400 rounded transition
-             duration-400 ease-in-out focus:text-gray-700 
-             focus:bg-white focus:border-slate-400 w-40 md:w-32"
-            style={{ borderRadius: 100 }}
-          ></input>
+          <div className="flex flex-col mt-2 md:flex-row md:items-center">
+            <label className="text-lg text-center font-bold mr-4">
+              Ubicación
+            </label>
+            <div className="text-center">
+              <input
+                type="text"
+                placeholder="Filtrar ubicación"
+                onChange={(event) => {
+                  setSearchBar(event.target.value);
+                }}
+                className="cursor-pointer text-sm text-gray-700
+          bg-white border border-gray-400 rounded transition
+           duration-400 ease-in-out focus:text-gray-700 
+           focus:bg-white focus:border-slate-400 w-40 md:w-32"
+                style={{ borderRadius: 100 }}
+              ></input>
+            </div>
           </div>
-        </div>
-
         </div>
       </div>
 
@@ -256,97 +262,101 @@ export default function Properties() {
                   id={listing.id}
                   listing={listing.data}
                 />
-              )).length > 0 ? ( //verifica que existan propiedades con los filtros ingresados
-              <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
-                {listings
-                  .filter((listing) => {
-                    //Se filtra por el numero de habitacions, ubicacion, precio
+              )).length > 0 ? (
+              //verifica que existan propiedades con los filtros ingresados
+              <>
+                <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+                  {listings
+                    .filter((listing) => {
+                      //Se filtra por el numero de habitacions, ubicacion, precio
 
-                    if (
-                      cantBedrooms == 0 &&
-                      priceRange == 0 &&
-                      cantBathrooms == 0
-                    ) {
-                      //filtra unicamente por ubicacion
+                      if (
+                        cantBedrooms == 0 &&
+                        priceRange == 0 &&
+                        cantBathrooms == 0
+                      ) {
+                        //filtra unicamente por ubicacion
 
-                      return listing.data.address
-                        .toLowerCase()
-                        .includes(searchBar.toLowerCase());
-                    } else if (cantBedrooms == 0 && cantBathrooms == 0) {
-                      //filtra por precio o  ubicacion-precio
-                      return (
-                        parseInt(listing.data.regularPrice) <= priceRange &&
-                        listing.data.address
+                        return listing.data.address
                           .toLowerCase()
-                          .includes(searchBar.toLowerCase())
-                      );
-                    } else if (priceRange == 0 && cantBathrooms == 0) {
-                      //filtra por habitacion o ubicacion-habitacion
+                          .includes(searchBar.toLowerCase());
+                      } else if (cantBedrooms == 0 && cantBathrooms == 0) {
+                        //filtra por precio o  ubicacion-precio
+                        return (
+                          parseInt(listing.data.regularPrice) <= priceRange &&
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase())
+                        );
+                      } else if (priceRange == 0 && cantBathrooms == 0) {
+                        //filtra por habitacion o ubicacion-habitacion
 
-                      return (
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase()) &&
-                        listing.data.bedrooms == cantBedrooms
-                      );
-                    } else if (cantBedrooms == 0 && priceRange == 0) {
-                      //filtra por baños o ubicacion-baños
+                        return (
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase()) &&
+                          listing.data.bedrooms == cantBedrooms
+                        );
+                      } else if (cantBedrooms == 0 && priceRange == 0) {
+                        //filtra por baños o ubicacion-baños
 
-                      return (
-                        listing.data.bathroms == cantBathrooms &&
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase())
-                      );
-                    } else if (priceRange == 0) {
-                      //filtra por habitaciones-baños o ubicacion-habitaciones-baños
+                        return (
+                          listing.data.bathroms == cantBathrooms &&
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase())
+                        );
+                      } else if (priceRange == 0) {
+                        //filtra por habitaciones-baños o ubicacion-habitaciones-baños
 
-                      return (
-                        listing.data.bedrooms == cantBedrooms &&
-                        listing.data.bathroms == cantBathrooms &&
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase())
-                      );
-                    } else if (cantBathrooms == 0) {
-                      //filtra por habitacions-precios o ubicacion-habitaciones-precios
-                      return (
-                        listing.data.bedrooms == cantBedrooms &&
-                        parseInt(listing.data.regularPrice) <= priceRange &&
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase())
-                      );
-                    } else if (cantBedrooms == 0) {
-                      //filtrar por banos-precio o ubicacion-banos-precio
+                        return (
+                          listing.data.bedrooms == cantBedrooms &&
+                          listing.data.bathroms == cantBathrooms &&
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase())
+                        );
+                      } else if (cantBathrooms == 0) {
+                        //filtra por habitacions-precios o ubicacion-habitaciones-precios
+                        return (
+                          listing.data.bedrooms == cantBedrooms &&
+                          parseInt(listing.data.regularPrice) <= priceRange &&
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase())
+                        );
+                      } else if (cantBedrooms == 0) {
+                        //filtrar por banos-precio o ubicacion-banos-precio
 
-                      return (
-                        listing.data.bathroms == cantBathrooms &&
-                        parseInt(listing.data.regularPrice) <= priceRange &&
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase())
-                      );
-                    } else {
-                      //filtra por todas las categorias
-                      return (
-                        listing.data.address
-                          .toLowerCase()
-                          .includes(searchBar.toLowerCase()) &&
-                        cantBedrooms == listing.data.bedrooms &&
-                        parseInt(listing.data.regularPrice) <= priceRange &&
-                        listing.data.bathroms == cantBathrooms
-                      );
-                    }
-                  })
-                  .map((listing) => (
-                    <ListingItem
-                      key={listing.id}
-                      id={listing.id}
-                      listing={listing.data}
-                    />
-                  ))}
-              </ul>
+                        return (
+                          listing.data.bathroms == cantBathrooms &&
+                          parseInt(listing.data.regularPrice) <= priceRange &&
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase())
+                        );
+                      } else {
+                        //filtra por todas las categorias
+                        return (
+                          listing.data.address
+                            .toLowerCase()
+                            .includes(searchBar.toLowerCase()) &&
+                          cantBedrooms == listing.data.bedrooms &&
+                          parseInt(listing.data.regularPrice) <= priceRange &&
+                          listing.data.bathroms == cantBathrooms
+                        );
+                      }
+                    })
+                    .map((listing) => (
+                      <ListingItem
+                        key={listing.id}
+                        id={listing.id}
+                        listing={listing.data}
+                      />
+                    ))}
+                </ul>
+                <Footer />
+              </>
             ) : (
               <p className="text-center mt-2  mb-2">
                 No hay propiedades disponibles con las caracteristicas
